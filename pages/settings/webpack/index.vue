938c1c20ba274a167f35c3ce4233fbb3cc382f8a
veaba <908662421@qq.com>
2018-09-15 22:17:24 +0800

    update webpack router

---
+++ pages/settings/webpack/index.vue
@@ -0,0 +1,25 @@
+<!------------------------
+ *@name Vue.js
+ *@author Jo.gel
+ *@date 9/15/2018
+ -------------------------->
+<template>
+  <div class="webpack-index">
+    {{msg}}
+  </div>
+</template>
+<script>
+  export default {
+    name: 'index',
+    components: {},
+    data () {
+      return {
+        msg: 'Hello world index VueJS'
+      }
+    },
+    methods: {}
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
